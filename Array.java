587e0819cec1bef33f6e25a92686d4de6cce03ea
Viraj Samson <virajsamson098@gmail.com>
2020-08-23 16:41:59 -0500

    Dynamic array

--- Array.java
+++ Array.java
@@ -40,23 +40,69 @@ public class Array <T> implements Iterable <T> {
 	}
 	
 	public void clear() {
-		for(int i=0; i<capacity; i++) {
-			buffer[i] = null;
+		for(int i=0; i<this.capacity; i++) {
+			this.buffer[i] = null;
 		}
-		length = 0;
+		this.length = 0;
 	}
 	
 	public void add(T value) {
-		if (length+1 > capacity) {
-			
+		if (this.length+1 > this.capacity) {
+			if(this.length == 0) {this.capacity = 1; }
+			else { this.capacity *= 2; }
+			T [] newArray = (T[]) new Object [this.capacity];
+			for(int i=0; i<this.length; i++) {
+				newArray[i] = this.buffer[i];
+			}
+			this.buffer = newArray;
 		}
-		buffer[length] = value;
+		
+		this.buffer[this.length++] = value;
+	}
+	
+	public void removeAt(int index) {
+		if (index >= this.length || index<0) {
+			throw new IndexOutOfBoundsException();
+		}
+		T[] newArray = (T[]) new Object[this.length-1];
+		for(int i=0, j=0; i<this.length; i++, j++) {
+			if(i == index) { j--;}
+			else newArray[j] = this.buffer[i];
+		}
+		this.buffer = newArray;
+		this.capacity = this.capacity -1;		
 	}
 	
 	@Override
 	public Iterator<T> iterator() {
-		// TODO Auto-generated method stub 
-		return null;
+		return new Iterator<T> () {
+			int index = 0; 
+			@Override
+			public boolean hasNext() {
+				return index < length;
+			}
+
+			@Override
+			public T next() {
+				return buffer[index++];
+			}
+			
+		};
+	}
+	
+	@Override
+	public String toString() {
+		if (this.length == 0) {
+			return "[]";
+		}
+		else {
+			StringBuilder string = new StringBuilder(this.length).append("[");
+			for (int i=0; i< this.length-1; i++) {
+				string.append(this.buffer[i] + ", ");
+			}
+			return string.append(this.buffer[this.length-1] + "]").toString();
+		}
+		
 	}
 	
 }
